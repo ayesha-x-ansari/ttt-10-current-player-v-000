7d7a14e0a169841641da8c33d968c43cee6f4f34
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-13 04:25:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,11 @@
 board = [" ", " ", " ", "X ", " ", "O ", " ", " ", " "]
 def turn_count(board)
   counter = 1
-  board.each do |color|
+  board.each do |board_element|
     if board = "X"   || board = "O"
       puts "Element text #{board}. Number of element occupied #{board_element}."
+      counter += 1
     end
   end
 end
-turn_count
+turn_count(board)
